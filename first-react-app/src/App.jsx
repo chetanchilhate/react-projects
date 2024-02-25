46be841e4fdec6b90260aeb4e2d65e140fe54621
Chetan Chilhate <chilhatechetan04@gmail.com>
2024-02-25 08:43:12 +0530

    carousel with freeze card initial

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,19 +1,93 @@
 import { useState } from "react";
-
 import "./App.css";
-import LikeButton from "./LikeButton";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+import { MdOutlineCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
 
 function App() {
-  const [masterName, setMasterName] = useState("Chetan");
+  const array = [
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+  ];
 
-  const changeMasterName = () => {
-    setMasterName("Chetan Chilhate");
+  const [current, setCurrent] = useState(1);
+
+  const [carousel, setCarousel] = useState(array.slice(current, current + 2));
+
+  const INTIAL_FREEZE_CAROUSEL = { arr: [], index: 0 };
+
+  const [freezeCarousel, setFreezeCarousel] = useState(INTIAL_FREEZE_CAROUSEL);
+
+  const EMPTY = "";
+
+  const fixedCard = array[0];
+
+  const [freezeCard, setFreezeCard] = useState(EMPTY);
+
+  const handleLeftClick = () => {
+    if (current >= carousel.length) {
+      setCurrent(current - 1);
+      setCarousel(array.slice(current - 1, current + 1));
+      if (freezeCard !== EMPTY) {
+        const { arr, index } = freezeCarousel;
+        setCarousel([freezeCard, arr[index - 1]]);
+        setFreezeCarousel({ arr, index: index - 1 });
+      }
+    }
+  };
+
+  const handleRightClick = () => {
+    if (current + carousel.length < array.length) {
+      setCurrent(current + 1);
+      setCarousel(array.slice(current + 1, current + 3));
+      if (freezeCard !== EMPTY) {
+        const { arr, index } = freezeCarousel;
+        setCarousel([freezeCard, arr[index + 1]]);
+        setFreezeCarousel({ arr, index: index + 1 });
+      }
+    }
+  };
+
+  const handleFreezeBtnClick = () => {
+    if (freezeCard === EMPTY) {
+      setFreezeCard(carousel[0]);
+      const filterValue = [fixedCard, carousel[0]];
+      const freezeCarouselArr = array.filter(
+        (item) => !filterValue.includes(item)
+      );
+      const freezeCarouselIndex = freezeCarouselArr.indexOf(carousel[1]);
+      setFreezeCarousel({ arr: freezeCarouselArr, index: freezeCarouselIndex });
+    } else {
+      setCarousel(array.slice(current, current + 2));
+      setFreezeCard(EMPTY);
+      setFreezeCarousel(INTIAL_FREEZE_CAROUSEL);
+    }
   };
 
   return (
     <>
-      <p className="read-the-docs">Your master name is {masterName}</p>
-      <LikeButton handleChangeMasterName={changeMasterName} />
+      <div className="container">
+        <div className="card">{fixedCard}</div>
+        <div className="card">{carousel[0]}</div>
+        <div className="card">{carousel[1]}</div>
+      </div>
+
+      <div className="nav-btn-container">
+        <FaArrowCircleLeft onClick={handleLeftClick} />
+        {freezeCard === EMPTY ? (
+          <MdCheckBoxOutlineBlank onClick={handleFreezeBtnClick} />
+        ) : (
+          <MdOutlineCheckBox onClick={handleFreezeBtnClick} />
+        )}
+        <FaArrowCircleRight onClick={handleRightClick} />
+      </div>
     </>
   );
 }
--- first-react-app/src/LikeButton.jsx
+++
@@ -1,5 +0,0 @@
-const LikeButton = ({ handleChangeMasterName }) => {
-  return <button onClick={handleChangeMasterName}>Like</button>;
-};
-
-export default LikeButton;
